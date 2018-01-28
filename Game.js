0dd3afd23988b07499bf1806fd8ebfbfee46d3d4
Craig Pullar <craigmichaelpullar@gmail.com>
2018-01-28 19:14:59 +0000

    Got all tests to pass

--- Game.js
+++ Game.js
@@ -3,51 +3,51 @@ const emptyBoard = () => [0,0,0,0,0,0,0,0,0];
 const Game = (board = emptyBoard()) => {    
     const getBoard = () => board;
 
-    const isBoardPositionInRange = boardPosition => (0 <= boardPosition && 9 > boardPosition);
-    const isBoardPositionTaken = boardPosition => board[boardPosition] != 0;
-    const throwError = error => { 
-        if(ERRORS.has(error)) throw new Error(ERRORS.get(error));
-        throw new Error('Invalid error thrown'); 
-    }
-    const wouldCreateInvalidState = (boardPosition, PLAYER) => {
-        const evalBoard = board.deepCopy();
-        evalBoard[boardPosition] = PLAYER;
-        return evalState(evalBoard) == STATES.get('INVALID');
-    }
-    const isValidMove = (boardPosition, PLAYER) => {
-        wouldCreateInvalidState(boardPosition, PLAYER) && throwError('MOVE_INVALID_STATE');
-        !isBoardPositionInRange(boardPosition) && throwError('BOARD_POSITION_RANGE');
-        isBoardPositionTaken(boardPosition) && throwError('BOARD_POSITION_TAKEN');
-        return true;
-    }
-
     const makeMove = (boardPosition, PLAYER) => {
+        const isBoardPositionInRange = boardPosition => (0 <= boardPosition && 9 > boardPosition);
+        const isBoardPositionTaken = boardPosition => board[boardPosition] != 0;
+        const throwError = error => { 
+            if(ERRORS.has(error)) throw new Error(ERRORS.get(error));
+            throw new Error('Invalid error thrown'); 
+        }
+        const wouldCreateInvalidState = (boardPosition, PLAYER) => {
+            const evalBoard = Object.assign(board.deepCopy(), { [boardPosition]: PLAYER});
+            return evalState(evalBoard) == STATES.get('INVALID');
+        }
+        const isValidMove = (boardPosition, PLAYER) => {
+            wouldCreateInvalidState(boardPosition, PLAYER) && throwError('MOVE_INVALID_STATE');
+            !isBoardPositionInRange(boardPosition) && throwError('BOARD_POSITION_RANGE');
+            isBoardPositionTaken(boardPosition) && throwError('BOARD_POSITION_TAKEN');
+            return true;
+        }
         isValidMove(boardPosition, PLAYER) && (board[boardPosition] = PLAYER);
     };
 
     
-    const areBoardIndexesTakenBySamePlayer = (evalBoard, indexArray) =>
-        indexArray.map((boardIndex, i) => evalBoard[boardIndex] > 0 && 
-            i ? (evalBoard[boardIndex] == evalBoard[indexArray[i-1]]) : true)
-        const isWinIndexArray = (indexArray, evalBoard) => 
-            areBoardIndexesTakenBySamePlayer(evalBoard, indexArray).every(valueIsTrue);
-        const valueIsTrue = val => val === true;  
-        const isWinState = evalBoard => 
-            POSSIBLE_WIN_INDEXES.map(isWinIndexArray.bind(evalBoard)).some(valueIsTrue);
+                          
 
-        const isActiveState = evalBoard => evalBoard.includes(0);
+    const evalState = (evalBoard = board.deepCopy()) => {
+        const areBoardIndexesTakenBySamePlayer = indexArray =>
+            indexArray.map((boardIndex, i) => evalBoard[boardIndex] > 0 && 
+                i ? (evalBoard[boardIndex] == evalBoard[indexArray[i-1]]) : true)
+        const isWinIndexArray = indexArray => 
+            areBoardIndexesTakenBySamePlayer(indexArray).every(valueIsTrue);
+        const valueIsTrue = val => val === true;  
+        const isWinState = () => 
+            POSSIBLE_WIN_INDEXES.map(isWinIndexArray).some(valueIsTrue);
+
+        const isActiveState = () => evalBoard.includes(0);
 
         const isNumberOfMovesFair = ([a,b]) => a-b < 2 && a-b > -2;
-        const getMovesCountArray = evalBoard => 
+        const getMovesCountArray = () => 
             evalBoard.deepCopy().reduce((accum, val) => 
                 val != 0 ? (accum[val-1]++, accum) : accum 
             , [0,0]);
-        const isValidState = evalBoard => isNumberOfMovesFair(getMovesCountArray(evalBoard));                           
+        const isValidState = () => isNumberOfMovesFair(getMovesCountArray()); 
 
-    const evalState = (evalBoard = board.deepCopy()) => {
-        if(!isValidState(evalBoard)) return STATES.get('INVALID');
-        else if(isWinState(evalBoard)) return STATES.get('WIN');
-        else if(isActiveState(evalBoard)) return STATES.get('ACTIVE');
+        if(!isValidState()) return STATES.get('INVALID');
+        else if(isWinState()) return STATES.get('WIN');
+        else if(isActiveState()) return STATES.get('ACTIVE');
         else return STATES.get('DRAW');
     }
 
